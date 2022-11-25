989b133c52283394262d279834960622035ad380
Mario Gómez <alu.124956@usj.es>
2022-11-26 00:00:49 +0100

    Add new components and try to see it in the home page

---
+++ jem_shocks/src/components/Home.jsx
@@ -0,0 +1,3 @@
+// Aquí es dónde tenemos que incluir todos los componentes que hay en la página de inicio
+import React from 'react';
+
--- jem_shocks/src/components/Navbar.jsx
+++ jem_shocks/src/components/Navbar.jsx
@@ -1,4 +1,28 @@
-// Importamos las librerías necesarias
 import React from 'react';
-// Importamos el css
+import { Link } from 'react-router-dom';
 import './Navbar.css';
+
+export class Navbar extends Component{
+    render(){
+        <div>
+            {/* Hacemos que si el usuario le da al logo del navbar, que le lleve a la página de inicio */}
+            <Link className="navbar-brand" to="/">JEM SHÖCKS</Link>
+            <div>
+                <ul className="navbar-nav">
+                    <li>
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link" to="/Tienda">Tienda</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link" to="/Informacion">Información</Link>
+                    </li>
+                    <li>
+                        <Link className="nav-link" to="/Contacto">Contacto</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    }
+}
