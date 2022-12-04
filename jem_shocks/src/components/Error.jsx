a635fbd5933c6b933d7d7540960df1e263089a74
Mario Gómez <alu.124956@usj.es>
2022-12-04 21:20:55 +0100

    add id to div

--- jem_shocks/src/components/Error.jsx
+++ jem_shocks/src/components/Error.jsx
@@ -4,8 +4,8 @@ import './Error.css';
 export class Error extends Component{
     render(){
         return(
-            <div>
-                <img src="/images/NotFound.png" alt="ERROR 404 - Not Found" />
+            <div id='error'>
+                <img src="/images/NotFound.png" alt="ERROR 404 - Not Found" id='Error404NotFound'/>
             </div>
         )
     }
