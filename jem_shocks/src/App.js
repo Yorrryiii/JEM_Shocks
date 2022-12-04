95a6a46f6c4df109c242b7d1160fb85410d356db
Mario Gómez <alu.124956@usj.es>
2022-12-04 12:53:17 +0100

    jorgete no t enfades :)

--- jem_shocks/src/App.js
+++ jem_shocks/src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 import Error from './components/Error';
-import { Home } from './components/Home';
-import { Navbar } from './components/Navbar';
+
 
 function App() {
   return (
-    <Navbar />
+    <Router>
+      <div>
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            
+            <Route path="*" element={<Error />} />
+          </Routes>
+      </div>
+    </Router>
     // <Router>
     //   <div>
     //     <Navbar/>
--- jem_shocks/src/components/Error.jsx
+++ jem_shocks/src/components/Error.jsx
@@ -1,12 +1,12 @@
-// Importamos las librerías de React
 import React, { Component } from 'react';
-// Importamos el css de Error
 import './Error.css';
 
 export class Error extends Component{
     render(){
         return(
-            <img src="../images/NotFound.png" alt="ERROR 404 - Not Found" />
+            <div>
+                <img src="/images/NotFound.png" alt="ERROR 404 - Not Found" />
+            </div>
         )
     }
 }
