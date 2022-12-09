a705042b1b1e6de5b9a5491189e60a761a24bcc0
Yorrryiii <94568320+Yorrryiii@users.noreply.github.com>
2022-12-09 12:28:37 +0100

    cambios de fuente espaciado y margenes en about us e incorporación de fotos de creadores

--- jem_shocks/src/components/About.jsx
+++ jem_shocks/src/components/About.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import './styles/About.css';
 
+
 export class About extends Component {
 
     render() {
         return (
             
             <div id='cuerpo'>
+                {/*Importamos la fuente POPPINS*/}
+                <link rel="stylesheet" href="https://use.typekit.net/cej1rqi.css"></link>
+                {/*Preloader*/}
                 <div id="preloader">
                     <div id="loader"></div>
                 </div>
+                {/*Header*/}
                 <div id='introduccion'>
                     <h5 id='sale'>WE SALE CUSTOME SOCKS</h5>
-                    <h1 id='aboutus'>About us</h1>
+                    <h1 id='aboutus'>About Us</h1>
                 </div>
-
+                {/*Web Content*/}
                 <div id='text'>
-                    <h1 id='titulo1'>Who are we?</h1>
+                    <h1 id='titulo1'>Why JEM Shöcks</h1>
                     <h2 id='cuerpo1'>
                         JEM SHÖCKS it is an online platform where you can buy different types of socks.
                         The shop offers a wide variety of styles, colours and sizes so that customers can choose the pair of socks that best suits their needs.
@@ -24,12 +29,48 @@ export class About extends Component {
                         The shop then sends the socks directly to the customer through the post.
                     </h2>
                 </div>
-
+                {/*Lineas divisorias*/}
                 <div className='row center' style={{ marginTop: '50px' }} >
                     <hr style={{ color: '#b48608', backgroundColor: '#b48608', height: 3, width: '70%' }} />
                     <hr style={{ color: '#b48608', backgroundColor: '#b48608', height: 3, width: '80%' }} />
                     <hr style={{ color: '#b48608', backgroundColor: '#b48608', height: 3, width: '70%' }} />
                 </div>
+
+                {/*Team*/}
+                <div class="photos">
+                    <div class="single-team-item">
+                        <div class="founders founders-1"></div>
+                        <h4>Mario Gómez <span>Founder</span></h4>
+                        <ul class="social-link-team">
+                            <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>                                
+                            <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="single-team-item">
+                        <div class="founders founders-2"></div>
+                        <h4>Jorge Serrano <span>Founder</span></h4>
+                        <ul class="social-link-team">
+                            <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+
+
+                    <div class="single-team-item">
+                        <div class="founders founders-3"></div>
+                        <h4>Elena Rozas <span>Founder</span></h4>
+                        <ul class="social-link-team">
+                            <li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+
+                </div>
+                
 
                <h2 id='titulo2'>
                 Will you join us to get to know us?
