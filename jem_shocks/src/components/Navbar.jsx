299688e3a8dbcf9fdeddae3a6d82648965748aa7
Mario Gómez <alu.124956@usj.es>
2022-12-15 15:55:04 +0100

    navbar arreglao

--- jem_shocks/src/components/Navbar.jsx
+++ jem_shocks/src/components/Navbar.jsx
@@ -30,13 +30,6 @@ export class Navbar extends Component {
                             <img src="/images/LOGO.png" alt="JEM SHÖCKS LOGO" />
                         </a>
 
-                        <div id='tabs'>
-                            <Link to="/JEM_SHOCKS/" id='home'>Home</Link>
-                            <Link to="/JEM_SHOCKS/shop" id='shop'>Shop</Link>
-                            <Link to="/JEM_SHOCKS/about" id='about'>About</Link>
-                            <Link to="/JEM_SHOCKS/contact" id='contact'>Contact</Link>
-                        </div> 
-                        
                         <div id='parteDcha'>
                             {/* Insertamos el carrito de la compra */}
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="lupa" viewBox="0 0 16 16">
@@ -48,6 +41,15 @@ export class Navbar extends Component {
                                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                             </svg>
                         </div>
+
+                        <div id='tabs'>
+                            <Link to="/JEM_SHOCKS/" id='home'>Home</Link>
+                            <Link to="/JEM_SHOCKS/shop" id='shop'>Shop</Link>
+                            <Link to="/JEM_SHOCKS/about" id='about'>About</Link>
+                            <Link to="/JEM_SHOCKS/contact" id='contact'>Contact</Link>
+                        </div> 
+                        
+
                     </ul>
                 </nav>
             </div>
