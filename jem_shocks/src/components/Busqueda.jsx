0c8d5facd5623f9fd858fe73f44746be55c20c55
Mario Gómez <alu.124956@usj.es>
2022-12-16 16:02:51 +0100

    componente busqueda

---
+++ jem_shocks/src/components/Busqueda.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/Busqueda.css';
+
+export class Busqueda extends Component{
+    render(){
+        return(
+            <div>
+                {/* Contenido de la página shop */}
+            </div>
+        )
+    }
+}
+
+export default Busqueda;
