66b66cd8d0adf1a58fe6ffa3170226a415fd0ec9
Mario Gómez <alu.124956@usj.es>
2023-01-02 14:05:26 +0100

    no message

--- jem_shocks/src/components/Home.jsx
+++ jem_shocks/src/components/Home.jsx
@@ -10,10 +10,22 @@ export class Home extends Component{
                     <h1 id='tituloPrincipal'>This is JEM SHÖCKS</h1>
                 </div>
                 <div id='segundaSeccion'>
-                    probando
-                    probando
-                    probando
-                    probando
+                    <table id='InfoPrincipal'>
+                        <tr>
+                            <td id='enviosGratis'>
+                                <h3>Free Shipping</h3>
+                                <p>When order over 30€</p>
+                            </td>
+                            <td id='ayuda'>
+                                <h3>24/7 Support</h3>
+                                <p>Get support all day</p>
+                            </td>
+                            <td id='devolución'>
+                                <h3>Refund</h3>
+                                <p>Get refund within 3 days!</p>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         )
