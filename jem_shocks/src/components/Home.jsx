e43ca6633b3606e3c143a59de75e74f309bb9d2b
Mario Gómez <alu.124956@usj.es>
2023-01-03 01:10:23 +0100

    no message

--- jem_shocks/src/components/Home.jsx
+++ jem_shocks/src/components/Home.jsx
@@ -52,7 +52,7 @@ export class Home extends Component {
                             <img src="/images/ejCalcetines1.png" alt="Socks 1" />
                             <h3>Fast Food</h3>
                             <p>3,95€</p>
-                            {/* Añadimos un boton que nos redireccione a la pagina de shop */}
+                            {/* Boton que nos redireccione a la pagina de shop */}
                             <button id='botonTienda'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
@@ -65,12 +65,24 @@ export class Home extends Component {
                             <img src="/images/ejCalcetines2.png" alt="Socks 1" />
                             <h3>Bananas & Monkeys</h3>
                             <p>4,95€</p>
+                            <button id='botonTienda'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                                </svg>
+                                <Link to="/JEM_SHOCKS/shop" id='VerTienda' onClick={'/JEM_SHOCKS/shop'}>View in shop</Link>
+                            </button>
                         </div>
 
                         <div id='ejCalcetines1'>
                             <img src="/images/ejCalcetines3.png" alt="Socks 1" />
                             <h3>Minions Skaters</h3>
                             <p>4,50€</p>
+                            <button id='botonTienda'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                                </svg>
+                                <Link to="/JEM_SHOCKS/shop" id='VerTienda' onClick={'/JEM_SHOCKS/shop'}>View in shop</Link>
+                            </button>
                         </div>
                     </div>
                 </div>
