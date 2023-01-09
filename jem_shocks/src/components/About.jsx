478378fbe5476c21745565ed5913fc982c752599
Yorrryiii <94568320+Yorrryiii@users.noreply.github.com>
2023-01-09 10:55:28 +0100

    no se que es pero lleva 2 semanas en mi ordenador jajajajaj

--- jem_shocks/src/components/About.jsx
+++ jem_shocks/src/components/About.jsx
@@ -25,7 +25,7 @@ export class About extends Component {
                         <tr>
                             <td class="title" colspan="4"><h4 id='titulo1'>Why <span id='titulo2'>JEM Shöcks</span></h4></td>
                             <td rowspan="5">
-                                <img class="fotoWhyJem" src="/images/AboutWhyJemFoto.jpg" alt="why-jem-shocks"/>
+                                <img class="fotoWhyJem" src="/images/AboutWhyJemFoto.jpg" alt="why-jem-shocks" />
                             </td>
                         </tr>
                         <tr>
@@ -36,7 +36,7 @@ export class About extends Component {
                             </td>
                             <td class="secTittle">Home Delivery</td>
                             <td class="tg-0lax" rowspan="2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-coin" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-coin" viewBox="0 0 16 16">
                                     <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z" />
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                                     <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
@@ -56,7 +56,7 @@ export class About extends Component {
                             </td>
                             <td class="secTittle">Custom Made</td>
                             <td class="tg-0lax" rowspan="2">
-                                
+
                             </td>
                             <td class="secTittle">Free Returns</td>
                         </tr>
@@ -123,9 +123,34 @@ export class About extends Component {
 
                 </div>
 
-                <h2 id='titulo2'>
-                    Will you join us to get to know us?
-                </h2>
+                <div class="partners">
+                <table class="tg">
+                    <thead>
+                        <tr>
+                            <th class="title" colspan="5"><h4 id='titulo1'>Our <span id='titulo2'>Partners</span></h4></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="tg-0lax">
+                            <img class="partner" src="/images/partner1.png" alt="why-jem-shocks" />
+                            </td>
+                            <td class="tg-0lax">
+                            <img class="partner" src="/images/partner2.png" alt="why-jem-shocks" />
+                            </td>
+                            <td class="tg-0lax">
+                            <img class="partner" src="/images/partner3.png" alt="why-jem-shocks" />
+                            </td>
+                            <td class="tg-0lax">
+                            <img class="partner" src="/images/partner4.png" alt="why-jem-shocks" />
+                            </td>
+                            <td class="tg-0lax">
+                            <img class="partner" src="/images/partner5.png" alt="why-jem-shocks" />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
             </div>
         )
     }
